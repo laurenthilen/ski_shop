65c971429b534eebcfa5d1f98c08f74030680f60
Lauren Thilen <laurenemick@Laurens-MacBook-Pro.local>
2024-01-18 11:41:48 -0800

    project set up

---
+++ API/Program.cs
@@ -0,0 +1,26 @@
+// Starter class - when we run dotnet run, all these methods are executed
+
+// Creates web app host
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the injection dependency container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline (middleware).
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
